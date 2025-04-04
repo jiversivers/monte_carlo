6dd318e5e2d9cb6b9cc1c217b307def06e1e9076
jiversivers <jdivers@uark.edu>
2025-04-04 11:56:50 -0500

    added 1d interp import

--- src/photon_canon/import_utils.py
+++ src/photon_canon/import_utils.py
@@ -2,7 +2,7 @@ try:
     import cupy as np
     from cupy import iterable
     from cupy.typing import NDArray
-    from cupyx.scipy.interpolate import RegularGridInterpolator
+    from cupyx.scipy.interpolate import RegularGridInterpolator, interp1d
 
     if not np.is_available():
         raise RuntimeError("CUDA not available; reverting to NumPy.")
@@ -10,6 +10,6 @@ except (ImportError, RuntimeError):
     import numpy as np
     from numpy import iterable
     from numpy.typing import NDArray
-    from scipy.interpolate import RegularGridInterpolator
+    from scipy.interpolate import RegularGridInterpolator, interp1d
 
     np.is_available = lambda: False  # Mock the `is_available` method for consistenc
