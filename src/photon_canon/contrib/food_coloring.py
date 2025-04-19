2b5951ca7b24496d0598939386775bb6b3339d34
jiversivers <jdivers@uark.edu>
2025-04-19 17:17:40 -0500

    Add pandas and numpy imports for data handling

--- src/photon_canon/contrib/food_coloring.py
+++ src/photon_canon/contrib/food_coloring.py
@@ -1,4 +1,6 @@
 import importlib.resources
+import pandas as pd
+import numpy as np
 
 with importlib.resources.open_text('photon_canon.data', "sample_food_coloring_absorption.csv") as f:
     df = pd.read_csv(f)
