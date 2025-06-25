48e9cf0b61833067461085573b0f3ffe319dbbcd
jiversivers <jdivers@uark.edu>
2025-06-25 18:48:55 -0500

    documentation and formatting

--- src/photon_canon/lut/lut.py
+++ src/photon_canon/lut/lut.py
@@ -1,6 +1,7 @@
 import itertools
 import logging
 import multiprocessing as mp
+import warnings
 from numbers import Real
 from typing import Optional, List, Union, Tuple, Type, Any, Callable
 
@@ -26,7 +27,8 @@ from ..utils import latest_simulation_id, CON
 
 c = CON.cursor()
 
-def single_sim(
+
+def _single_sim(
     args: Tuple[
         System, Medium, Photon, list[str, ...], list[float, ...], Optional[str]
     ],
@@ -113,13 +115,13 @@ def generate_lut(
             if pbar:
                 results = list(
                     tqdm(
-                        pool.imap(single_sim, params),
+                        pool.imap(_single_sim, params),
                         total=len(params),
                         desc=f"Sim ID: {simulation_id}",
                     )
                 )
             else:
-                results = list(pool.imap(single_sim, params))
+                results = list(pool.imap(_single_sim, params))
         except Exception as e:
             logging.debug(f"Multiprocessing failed: {e}")
             results = []
@@ -135,6 +137,26 @@ def generate_lut(
 
 
 class LUT(BaseModel):
+    """
+    Lookup table class that acts as an interface to generated LUT data. This class includes callability with one or
+    multiple parameters, with the parameter call order set at LUT object creation by `dimensions`. When called, the LUT
+    will automatically handle extrapolation based on its settings and return the response value accordingly. The LUT
+    surface can be smoothed by passing a smoohting_fn, which is applied to the grid at interpolation time. The LUT also
+    provides several methods to visualize or access the underlying LUT data. Finally, class/instance methods of the LUT
+    class provide a simplified interface for querying the available information across multiple simulations.
+
+    :param simulation_id: The ID of the generated LUT to access.
+    :type simulation_id: int
+    :param dimensions: The dimensions to consider at lookup in the LUT. The order will be used for calls. Default: 'mu_s', 'mu_a', 'g'.
+    :type dimensions: List[str]
+    :param extrapolate: Whether to extrapolate outside the table bounds. Overrideable at call time as a kwarg. Default: False.
+    :type extrapolate: bool
+    :param scale: The scale to compare responses against to normalize (if needed). Default: 1.
+    :type scale: float
+    :param smoothing_fn: A function that will be applied to the response grid. Optional. Default: None.
+    :type smoothing_fn: callable
+    """
+
     simulation_id: int = latest_simulation_id
     dimensions: List[Dimensions] = [Dimensions.MU_S, Dimensions.MU_A, Dimensions.G]
     extrapolate: bool = False
@@ -151,6 +173,15 @@ class LUT(BaseModel):
     def __call__(
         self, *values: Union[Real, np.ndarray], extrapolate: bool = None
     ) -> Union[Real, np.ndarray]:
+        """
+        Callable interface that looks up the response value according to the inputs along the object's set dimensions.
+        If only the number of arguments provided is fewer than the tables dimensions, all response points for that set
+        of arguments will be returned.
+
+        :param values: Query points of the LUT,order by the `dimensions` of the object.
+        :param extrapolate: Whether to extrapolate for values outside the table's bounds.
+        :return response: LUT response at the queried values.
+        """
         if not isinstance(values, tuple):
             values = (values,)
         if not len(values) <= len(self.dimensions):
@@ -191,6 +222,9 @@ class LUT(BaseModel):
 
     @property
     def interpolator(self) -> RegularGridInterpolator:
+        """
+        :return: The interpolator to use for interpolation of the response.
+        """
         if self._interpolator is None:
             query = f"SELECT {', '.join(self.dimensions)}, output FROM mclut WHERE simulation_id={self.simulation_id}"
             c.execute(query)
@@ -225,16 +259,33 @@ class LUT(BaseModel):
         return self._interpolator
 
     @classOrInstanceMethod
-    def to_pandas(self: Optional["LUT"], cls: Type["LUT"], simulation_id: Optional[int] = None) -> pd.DataFrame:
+    def to_pandas(
+        self: Optional["LUT"], cls: Type["LUT"], simulation_id: Optional[int] = None
+    ) -> pd.DataFrame:
+        """
+        A class or instance method that gets a dataframe of the LUT data.
+        :param simulation_id: The id of the simulation to get.
+        :return: DataFrame of the LUT data.
+        """
         simulation_id = _get_sim_id(self, simulation_id)
         query = f"SELECT mu_s, mu_a, g, output FROM mclut WHERE simulation_id = {simulation_id}"
         df = pd.read_sql_query(query, CON)
         if self.smoothing_fn is not None:
-            df["output"] = self.smoothing_fn(df["output"].values)
+            warnings.warn(
+                "Smoothing is not applied to output dataframe. Reshape and apply manually if desired.",
+                stacklevel=2,
+            )
         return df
 
     @classOrInstanceMethod
-    def list_available(self: Optional["LUT"], cls: Type["LUT"], portion: str = "ALL") -> List[int]:
+    def list_available(
+        self: Optional["LUT"], cls: Type["LUT"], portion: str = "ALL"
+    ) -> List[int]:
+        """
+        A class or instance method that gets a list of available LUT simulation IDs.
+        :param portion: What portion of the available LUT table to get. Available options are 'HEAD', 'TAIL', and 'ALL'. Default: 'ALL'.
+        :return: A lsit of the available LUTs simulation IDs.
+        """
         c.execute(
             """
         SELECT DISTINCT id FROM mclut_simulations
@@ -254,7 +305,14 @@ class LUT(BaseModel):
         return available[portion]
 
     @classOrInstanceMethod
-    def get_layer_data(self: Optional["LUT"], cls: Type["LUT"], simulation_id: Optional[int] = None) -> pd.DataFrame:
+    def get_layer_data(
+        self: Optional["LUT"], cls: Type["LUT"], simulation_id: Optional[int] = None
+    ) -> pd.DataFrame:
+        """
+        A class or instance method that gets a dataframe of the LUT layer data.
+        :param simulation_id: The id of the simulation to get data from. Optional. If None, all available layer dats is retrieved. Default: None.
+        :return: A dataframe of the LUT layer data.
+        """
         simulation_id = _get_sim_id(self, simulation_id)
         query = f"SELECT * FROM fixed_layers WHERE simulation_id = {simulation_id}"
         df = pd.read_sql_query(query, CON)
@@ -262,8 +320,18 @@ class LUT(BaseModel):
 
     @classOrInstanceMethod
     def get_metadata(
-        self: Optional["LUT"], cls: Type["LUT"], simulation_id: Optional[int] = None, portion: str = "ALL"
+        self: Optional["LUT"],
+        cls: Type["LUT"],
+        simulation_id: Optional[int] = None,
+        portion: str = "ALL",
     ) -> pd.DataFrame:
+        """
+        A class or instance method that gets a dataframe of the LUT simulation metadata. Note, this dataframe will not include smoothing, regardless of self.smoothing_fn. For smoothed LUT array outputs, see :ref:`surface`.
+
+        :param simulation_id: The id of the simulation to get the metadata from. Optional. If None, all available metadata is retrieved, and returned portion depends on `portion`. Default: None.
+        :param portion: What portion of the available metadata to return. Available options are 'HEAD', 'TAIL', and 'ALL'. Default: 'ALL'.
+        :return: A dataframe of the LUT simulation metadata.
+        """
         query = f"SELECT * FROM mclut_simulations"
         portion = ListPortion[portion.upper()].value
         simulation_id = _get_sim_id(self, simulation_id, set_default=False)
@@ -273,11 +341,18 @@ class LUT(BaseModel):
         return df[portion]
 
     def surface(self) -> Tuple[Real, Real, Real]:
+        """
+        .. _surface:
+        Get the surface information for the LUT. Returns all unique values of each of the first 2 dimensions and the grid of responses at those dimensions. This allows for quick and easy visualization using ax.plot_wireframe(X, Y, Z). Note, this surface will include smoothing if self.smoothing_fn is not None.
+
+        :return: both independent variables and the dependent response surface.
+        """
         df = self.to_pandas(self.simulation_id)
         x = df[self.dimensions[0]].unique()
         y = df[self.dimensions[1]].unique()
         X, Y = np.meshgrid(x, y, indexing="ij")
         Z = np.reshape(df["output"], (len(x), len(y)))
-        if hasattr(self, "scale"):
-            Z /= self.scale
+        Z /= self.scale
+        if self.smoothing_fn is not None:
+            Z = self.smoothing_fn(Z)
         return X, Y, Z
