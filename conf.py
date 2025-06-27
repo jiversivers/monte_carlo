49ba9213ea2660cce1654bfa99c9fa3e9f233030
jiversivers <jdivers@uark.edu>
2025-06-27 09:14:11 -0500

    documentation autogenerated

---
+++ conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Photon Canon'
+copyright = '2025, Jiversivers'
+author = 'Jiversivers'
+release = '2.1.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_autodoc_typehints",
+    "myst_parser"
+]
+
+autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "show-inheritance": True,
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,22 @@
+.. Photon Canon documentation master file, created by
+   sphinx-quickstart on Fri Jun 27 07:58:00 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Photon Canon documentation
+==========================
+A python toolbox for in-depth 3D Monte Carlo simulation with custom systems of planar mediums, custom beams and detectors.
+
+.. autosummary::
+   :toctree: generated
+   :recursive:
+
+   photon_canon
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   README.md
+   generated/photon_canon
+
